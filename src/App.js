23e028ca88cb92dcbcf45575ff660461fd34990b
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2021-10-18 03:16:52 -0500

    Finish the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState, useEffect } from "react";
+import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 
 function App() {
+  // Traer las citas que estan en el localStorage
+  let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+
+  // Funcion que nos sirve para actulizar el state de citas - dentro de esta ejecutamos invocamos la funcion que actualiza la citas
+  const crearCita = (cita) => {
+    // Actualizamos el state de citas, obteniedo los valores anterios y agregando la nueva cita
+    guardarCitas([...citas, cita]);
+  };
+
+  // State para las citas
+  const [citas, guardarCitas] = useState(citasIniciales);
+
+  // UseEffect para actulizar nuestro localStorage guardan lo que tenemos en nuestra variable citas
+  useEffect(() => {
+    localStorage.setItem("citas", JSON.stringify(citas));
+  }, [citas]);
+
+  // Funcion que elimina una cita por su id
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter((cita) => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  };
+
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus citas";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Administrador de Pacientes Canicos</h1>
+      <div className="container">
+        <div className="one-half column">
+          {/* prettier-ignore */}
+          <Formulario
+            crearCita={crearCita}
+          />
+        </div>
+        <div className="one-half column">
+          <h2>{titulo}</h2>
+          {/* Iteraos el array con map y por cada cita retornamos un componente */}
+          {citas.map((cita) =>
+            /* prettier-ignore */
+            <Cita
+              key={cita.id}
+              cita={cita}
+              eliminarCita={eliminarCita}
+            />
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
